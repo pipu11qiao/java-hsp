8063855f35754f370091f25f2e1fb0b5f8035048
wangyong16 <wangyong16@xiaomi.com>
2022-10-26 09:01:58 +0800

    feat: update

---
+++ java-lxf/base/src/webDemo/ServletBase.java
@@ -0,0 +1,2 @@
+package webDemo;public class ServletBase {
+}
---
+++ projects/web-sevlet-hello/src/main/java/org/example/HelloServlet.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter pw = resp.getWriter();
+        pw.write("<h1>Hello, world!</h1>");
+        pw.flush();
+    }
+}
