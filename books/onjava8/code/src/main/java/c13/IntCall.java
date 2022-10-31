4c8a931ff657ccef256cb0af67fb9a9ff8b1f7fb
wangyong16 <wangyong16@xiaomi.com>
2022-10-31 09:09:39 +0800

    feat: update

---
+++ books/onjava8/code/src/main/java/c13/IntCall.java
@@ -0,0 +1,5 @@
+package c13;
+
+interface IntCall {
+    int call(int arg);
+}
---
+++ books/onjava8/code/src/main/java/c13/RecursiveFactorial.java
@@ -0,0 +1,9 @@
+package c13;
+
+import java.util.function.IntFunction;
+
+//public class RecursiveFactorial {
+//    public static void main(String[] args) {
+//        IntFunction fact = (int n) -> n == 0 ? 1 : n * fact(n - 1);
+//    }
+//}
